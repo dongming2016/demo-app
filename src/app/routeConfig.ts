6383671d136894e252654048a4621f9749172db4
chenmin <350376345@qq.com>
2018-05-08 16:05:50 +0800

    增加重定向的路由，并配置导航。

--- src/app/routeConfig.ts
+++ src/app/routeConfig.ts
@@ -2,5 +2,6 @@ import { HerosComponent } from './heros/heros.component'
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+    { path:'', pathMatch:'prefix', redirectTo: 'heroes'},
     { path: 'heroes', component: HerosComponent }
 ]
